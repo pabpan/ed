7ae0c588b80c6a33451ba821b0d28a05db1ca9bb
pabpan <pabpan@alu.iesserpis.org>
2019-10-24 12:13:12 +0200

    Arrays

--- Vector/src/iesserpis/ed/Vector.java
+++ Vector/src/iesserpis/ed/Vector.java
@@ -1,19 +1,32 @@
+//PABLO SUÁREZ
+//24-10-19
+//VECTORES EN JAVA
+
 package iesserpis.ed;
 
 public class Vector {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		int[] v = {17,12,15,9,14};
-		for (int index=0; index<5; index++)
-		System.out.printf("v[%s](Elemento) = %s %n(valor)", index, v[ index ]);
+		int[] v = new int[] {17,12,15,9,14};
 		
-		/*
-		int[] v = {17,12,15,9,14};
-		System.out.println("v[0] = "+v[0]);
-		System.out.println("v[1] = "+v[1]);
-		*/
+		for (int index=0; index<5; index++)
+		{
+			if (v[index]==9)
+				System.out.printf("está en la posición: "+index);
+		}
+			
+			//System.out.printf("v[%s](Elemento) = %s %n(valor)", index, v[ index ]);
+		
+		//int index=0;
+		
+		//while (index<5) {
+		//System.out.println(index);
+		//index++;
+		//}
+		
+		
+
 		
 	}
 
