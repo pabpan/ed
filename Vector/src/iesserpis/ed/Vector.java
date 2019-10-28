efb30025c81e6af4ba06f322fd16bbec22d85728
pabpan <pabpan@alu.iesserpis.org>
2019-10-28 14:02:24 +0100

    Vectores

--- Vector/src/iesserpis/ed/Vector.java
+++ Vector/src/iesserpis/ed/Vector.java
@@ -8,13 +8,13 @@ public class Vector {
 
 	public static void main(String[] args) {
 
-		int[] v = new int[] {17,12,15,9,14};
+		//int[] v = new int[] {17,12,15,9,14};
 		
-		for (int index=0; index<5; index++)
-		{
-			if (v[index]==9)
-				System.out.printf("está en la posición: "+index);
-		}
+		//for (int index=0; index<5; index++)
+		//{
+		//	if (v[index]==9)
+		//		System.out.printf("está en la posición: "+index);
+		//}
 			
 			//System.out.printf("v[%s](Elemento) = %s %n(valor)", index, v[ index ]);
 		
@@ -26,8 +26,24 @@ public class Vector {
 		//}
 		
 		
-
+		int[] v = new int[] {17, 12, 15, 9, 14, 9};
+		int x=9;
+		int posicion;
 		
-	}
+		posicion = indexOf(v, x);
+		
+		System.out.println("index=" + posicion);
+		System.out.println("FIN");
+		
+		}
+		
+		public static int indexOf (int[] v, int numero) {
+			int index=0;
+			while (index < v.length && v[index] != numero)
+				index++;
+				if (index==v.length)
+					return -1;
+			return index;
+		}
 
-}
+	}
