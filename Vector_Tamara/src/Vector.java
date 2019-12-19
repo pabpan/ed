98706a22b5b267671e217d6a76bc34c3193c006e
pabpan <pabpan@alu.iesserpis.org>
2019-12-19 09:15:27 +0100

    Navidad

--- Vector_Tamara/src/Vector.java
+++ Vector_Tamara/src/Vector.java
@@ -1,41 +1,52 @@
-
+import java.util.Arrays;
 
 public class Vector {
 
 	public static void main(String[] args) {
 
-		int vector[]= {21,14,9,18,3};
+		int vector[] = { 21, 14, 9, 18, 3 };
 		ordenar(vector);
 		for (int i = 0; i < vector.length; i++) {
-			System.out.print(vector[i]+" ");	
-			if (i<vector.length-1) {
-				System.out.print(", ");	
+			System.out.print(vector[i] + " ");
+			if (i < vector.length - 1) {
+				System.out.print(", ");
 			}
 		}
-			
+
 	}
-	
+
 	public static int maximo(int[] v) {
-		
+		if (v.length == 0)
+			throw new IllegalArgumentException();
 		int maximo = v[0];
 		for (int i = 0; i < v.length; i++) {
 			if (v[i] > maximo)
-				maximo = v[i];		
-		}return maximo;
-		
-		
+				maximo = v[i];
+		}
+		return maximo;
+
 	}
 
 	public static int[] ordenar(int[] v) {
+		if (v.length == 0)
+			throw new IllegalArgumentException();
 		int aux;
 		for (int j = 0; j < v.length; j++) {
-			for (int i = 0; i < v.length-j-1; i++) {
-				if (v[i]>v[i+1]) {
-				aux = v[i+1];
-				v[i+1]=v[i];
-				v[i]=aux;
-			}}
-				
-		}return v;	
+			for (int i = 0; i < v.length - j - 1; i++) {
+				if (v[i] > v[i + 1]) {
+					aux = v[i + 1];
+					v[i + 1] = v[i];
+					v[i] = aux;
+				}
+			}
+
+		}
+		return v;
 	}
+
+	public static int[] ordenar2(int[] v) {
+		Arrays.sort(v);
+		return v;
+	}
+
 }
--- Vector_Tamara/src/VectorTest.java
+++ Vector_Tamara/src/VectorTest.java
@@ -1,6 +1,7 @@
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class VectorTest {
@@ -13,6 +14,9 @@ class VectorTest {
 		assertEquals(21, Vector.maximo(new int[] { 21, 8, 9, 21 }));
 		assertEquals(21, Vector.maximo(new int[] { 8, 9, 16, 21 }));
 		assertEquals(21, Vector.maximo(new int[] { 0, 21 }));
+		
+		//Caso del vector vacío
+		Assertions.assertThrows(IllegalArgumentException.class, () -> Vector.maximo(new int[] {}));
 	}
 
 	@Test
@@ -20,6 +24,13 @@ class VectorTest {
 
 		assertArrayEquals(new int[] { 1, 2, 3, 5 }, Vector.ordenar(new int[] { 3, 5, 2, 1 }));
 		assertArrayEquals(new int[] { -1, 3, 3, 5 }, Vector.ordenar(new int[] { 5, 3, -1, 3 }));
+		Assertions.assertThrows(IllegalArgumentException.class, () -> Vector.maximo(new int[] {}));
+	}
+	
+	void ordenar2() {
+
+		assertArrayEquals(new int[] { 1, 2, 3, 5 }, Vector.ordenar2(new int[] { 3, 5, 2, 1 }));
+		assertArrayEquals(new int[] { -1, 3, 3, 5 }, Vector.ordenar2(new int[] { 5, 3, -1, 3 }));
 	}
 	
 }
