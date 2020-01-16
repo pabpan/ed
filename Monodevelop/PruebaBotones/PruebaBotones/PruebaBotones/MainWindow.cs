2e9be60c4e3229669d3ac46fd780ee13f824c996
pabpan <pabpan@alu.iesserpis.org>
2020-01-16 08:55:44 +0100

    Entorno gráfico

---
+++ Monodevelop/PruebaBotones/PruebaBotones/PruebaBotones/MainWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnBSaludarClicked(object sender, EventArgs e)
+    {
+        MessageDialog md = new MessageDialog(null, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, "HOLA");
+        md.Run();
+        md.Destroy();
+    }
+}
