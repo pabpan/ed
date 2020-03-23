9a4ab113c7205114c64b22da71f932d50f9062a6
pabpan <pabpan@alu.iesserpis.org>
2020-03-23 19:02:49 +0100

    ACalculadora

--- android studio/app/src/main/java/Package/miprimeraapp/MainActivity.java
+++ android studio/app/src/main/java/Package/miprimeraapp/MainActivity.java
@@ -39,6 +39,50 @@ public class MainActivity extends AppCompatActivity {
 
         /*Mostrar el resultado y convertir a String de nuevo*/
         tv1.setText(String.valueOf(suma));
+    }
 
+    /*MÉTODO RESTA*/
+
+    public void restar(View View){
+
+        /*Definimos las variables*/
+        int num1 = Integer.parseInt(et1.getText().toString());
+        int num2 = Integer.parseInt(et2.getText().toString());
+
+        /*Realizamos la operación*/
+        int resta = num1 - num2;
+
+        /*Mostrar el resultado y convertir a String de nuevo*/
+        tv1.setText(String.valueOf(resta));
+    }
+
+    /*MÉTODO MULTIPLICAR*/
+
+    public void multiplicar(View View){
+
+        /*Definimos las variables*/
+        int num1 = Integer.parseInt(et1.getText().toString());
+        int num2 = Integer.parseInt(et2.getText().toString());
+
+        /*Realizamos la operación*/
+        int multiplicacion = num1 * num2;
+
+        /*Mostrar el resultado y convertir a String de nuevo*/
+        tv1.setText(String.valueOf(multiplicacion));
+    }
+
+    /*MÉTODO DIVIDIR*/
+
+    public void dividir(View View){
+
+        /*Definimos las variables*/
+        int num1 = Integer.parseInt(et1.getText().toString());
+        int num2 = Integer.parseInt(et2.getText().toString());
+
+        /*Realizamos la operación*/
+        int division = num1 / num2;
+
+        /*Mostrar el resultado y convertir a String de nuevo*/
+        tv1.setText(String.valueOf(division));
     }
 }
